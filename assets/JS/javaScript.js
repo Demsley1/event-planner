f8a3298c28f4870a0059744fa55306ef9d7ac149
Denva <emsley3684@gmail.com>
2021-11-07 17:44:44 -0500

    added save and get functions for data in local storage

--- assets/JS/javaScript.js
+++ assets/JS/javaScript.js
@@ -27,15 +27,18 @@ $(document).ready(function() {
         var textVal = $(this).siblings(".description").val();
         var timeVal = $(this).parent().attr("id");
         localStorage.setItem(timeVal, textVal);
+
     });
 
     auditTimeBlock();
-    
+    var savedTasks = $("#8", ".description" ).append().val(localStorage.getItem());
+    return savedTasks;
 });
 
 var getTime = function() {
     var showDate = "<div>" + moment().format('MMMM Do YYYY'); + "</div>";
     $( "#currentDay" ).append(showDate);
 }
+
 
 getTime();
