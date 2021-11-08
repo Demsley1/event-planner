8247724b676d17474a691feac3f6a8ca5e56ad01
Denva <emsley3684@gmail.com>
2021-11-08 08:22:41 -0500

    "added ability to get local storage value"

--- assets/JS/javaScript.js
+++ assets/JS/javaScript.js
@@ -1,7 +1,9 @@
 var hour = document.querySelectorAll("#currentHour");
 var containerEl = document.querySelector(".container");
 
+// only once the html document has fully loaded start running these functions.
 $(document).ready(function() {
+    // function to color code time block based on past, present, and future.
     var auditTimeBlock = function() {
         // get current hour from 
         var hourNow = moment().hour();
@@ -23,6 +25,7 @@ $(document).ready(function() {
 
     };
 
+    // on save button click store the key and value of the text based on the timeblock id.
     $(".saveBtn").on("click", function () {
         var textVal = $(this).siblings(".description").val();
         var timeVal = $(this).parent().attr("id");
@@ -30,11 +33,18 @@ $(document).ready(function() {
 
     });
 
-    auditTimeBlock();
-    var savedTasks = $("#8", ".description" ).append().val(localStorage.getItem());
-    return savedTasks;
+    // function to retrieve and show stored data from local storage to a timeblock
+    function getData() {
+        var savedTasks = localStorage.getItem((8));
+        $("#8 .description").append(savedTasks)
+        console.log(savedTasks);
+    }
+    
+    auditTimeBlock();    
+    getData();
 });
 
+// shows date at top of page in DOM
 var getTime = function() {
     var showDate = "<div>" + moment().format('MMMM Do YYYY'); + "</div>";
     $( "#currentDay" ).append(showDate);
